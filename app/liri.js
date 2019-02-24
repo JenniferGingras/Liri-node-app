06139c773340fb00f979727f4bfd579390e1272f
Jennifer Gingras <jennifergingras@tests-MacBook-Pro-2.local>
2019-02-23 20:13:29 -0500

    movie function works

--- app/liri.js
+++ app/liri.js
@@ -9,20 +9,19 @@ var axios = require("axios");
 // ----------------------
 // command for search is node liri.js concert-this <insert artist/band name>
 var concert = function () {
-  var artist = process.argv[3];
-  // var artist = process.argv.slice(3).join("+");
+  var artist = process.argv.slice(3).join("+");
+  // console.log(artist);
   // store api url in variable
   var URL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
-  // var URL = "https://rest.bandsintown.com/artists/Halestorm/events?app_id=codingbootcamp"
   // use axios.get with query variable
   if (!artist) {
-    console.log("\n----Try again, please. Remember to choose an artist or band.----\n")
+    console.log("\n-!! Try again, please. Remember to type in an artist or band. !!-\n")
   }
   axios.get(URL).then(function (response) {
     // console.log data (name of venue, venue location, date of event (use moment- blech! - to format as MM/DD/YYYY))
     // NB - put each piece of data on a separate line
     var data = response.data[0];
-    var time = moment(data.datetime).format("LLLL");
+    var time = moment(data.datetime).format("MMMM Do YYYY, h:mm:ss a");
     console.log("\n----Upcoming Concert Info----\n" +
       "\nVenue: " + data.venue.name +
       "\nLocation: " + data.venue.city + ", " + data.venue.country +
@@ -69,33 +68,39 @@ var concert = function () {
 
 
 
-// search for movie
+// SEARCH FOR A MOVIE
+// ---------------------
 // command for search is node liri.js movie-this <insert movie name>
 var movie = function (query) {
   var movieTitle = process.argv.slice(3).join("+");
+  console.log(movieTitle);
   // store api url in variable
   var URL = "http://www.omdbapi.com/?t=" + movieTitle + "&apikey=trilogy&"
   // var nobody = "http://www.omdbapi.com/?t=mr+nobody&apikey=trilogy&"
   // create default for no input
   if (!movieTitle) {
-    movieTitle = "Mr+Nobody";
+    // movieTitle = "Mr+Nobody";
+    URL = "http://www.omdbapi.com/?t=mr+nobody&apikey=trilogy&"
+    console.log("\n-!! You didn't pick a movie, so we picked for you. !!-")
   }
   // use axios.get with query variable
-  axios.get(URL).then(function (response, err) {
-      if (err) {
-        return console.log("Error: " + err);
+  axios.get(URL).then(function (response) {
+      var data = response.data;
+      if (data.Title === undefined) {
+        console.log("\n--Could not find title. Check your spelling and try again.--\n\n")
+      } else {
+        console.log("\n----Movie Info----\n" +
+          "\nTitle: " + data.Title +
+          "\nYear Released: " + data.Year +
+          "\nIMDB Rating: " + data.imdbRating +
+          // "\nRotten Tomatoes Rating: " + data.Ratings[1].Value +
+          "\nCountry: " + data.Country +
+          "\nLanguage: " + data.Language +
+          "\nSummary: " + data.Plot +
+          "\nCast: " + data.Actors +
+          "\n\n");
+        // console.log data (title, year, IMDB rating, Rotten Tomatoes rating, country, language, plot, actors)
       }
-      data = response.data;
-      console.log("\n----Movie Info----\n" +
-        "\nTitle: " + data.Title +
-        "\nYear Released: " + data.Year +
-        "\nIMDB Rating: " + data.imdbRating +
-        "\nRotten Tomatoes Rating: " + data.Ratings[1].Value +
-        "\nCountry: " + data.Country +
-        "\nLanguage: " + data.Language +
-        "\nSummary: " + data.Plot +
-        "\nCast: " + data.Actors);
-      // console.log data (title, year, IMDB rating, Rotten Tomatoes rating, country, language, plot, actors)
     })
     .catch(function (error) {
       console.log(error);
