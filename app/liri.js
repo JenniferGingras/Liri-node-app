8012a4c910e78bec5b19015ad2b6bff5de4ef230
Jennifer Gingras <jennifergingras@tests-MacBook-Pro-2.local>
2019-02-23 19:31:59 -0500

    concert function works

--- app/liri.js
+++ app/liri.js
@@ -1,89 +1,108 @@
 require("dotenv").config();
 var keys = require("./keys.js");
-
+var moment = require("moment");
 var Spotify = require('node-spotify-api');
-
-
-
-console.log("I'm here!");
-
 var axios = require("axios");
-// search for Bands in Town events
+
+
+// BANDS IN TOWN SEARCH
+// ----------------------
 // command for search is node liri.js concert-this <insert artist/band name>
 var concert = function () {
   var artist = process.argv[3];
   // var artist = process.argv.slice(3).join("+");
   // store api url in variable
-  // var queryBands = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
-  var queryBands = "https://rest.bandsintown.com/artists/Halestorm/events?app_id=codingbootcamp"
+  var URL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+  // var URL = "https://rest.bandsintown.com/artists/Halestorm/events?app_id=codingbootcamp"
   // use axios.get with query variable
-  axios.get(queryBands).then(
-    function (response) {
-      // console.log(response);
-      console.log("Venue: " + response.data.venue.name);
-    });
-
-  // console.log data (name of venue, venue location, date of event (use moment- blech! - to format as MM/DD/YYYY))
-  // NB - put each piece of data on a separate line
-}
-
-// Search for a song
-// command for search is node liri.js spotify-this-song <insert song name>
-// function 'spotify-this-song'
-var spotify = new Spotify(keys.spotify);
-var songTitle = process.argv.slice(3).join(" ");
-var spotifySong = function (query) {
-  if (!query) {
-    console.log("if")
-    spotify.search({
-        type: "track",
-        query: "The Sign"
-      })
-      .then(function (response) {
-        console.log(response)
-      });
-  } else {
-    console.log.apply("else")
-    spotify.search({
-        type: "track",
-        query: query
-      })
-      .then(function (response) {
-        console.log(response);
-        // console.log data (artist, song's name, preview lin of song from Spotify, album name)
-      });
+  if (!artist) {
+    console.log("\n----Try again, please. Remember to choose an artist or band.----\n")
   }
+  axios.get(URL).then(function (response) {
+    // console.log data (name of venue, venue location, date of event (use moment- blech! - to format as MM/DD/YYYY))
+    // NB - put each piece of data on a separate line
+    var data = response.data[0];
+    var time = moment(data.datetime).format("LLLL");
+    console.log("\n----Upcoming Concert Info----\n" +
+      "\nVenue: " + data.venue.name +
+      "\nLocation: " + data.venue.city + ", " + data.venue.country +
+      "\nTime: " + time +
+      "\n\n");
+  });
+
+
 }
+
+// // SEARCH FOR A SONG WITH SPOTIFY NODE API
+// // ----------------------------------------
+// // command for search is node liri.js spotify-this-song <insert song name>
+// var spotify = new Spotify(keys.spotify);
+// var songTitle = process.argv.slice(3).join(" ");
+// var spotifySong = function (query) {
+//   if (!query) {
+//     console.log("if")
+//     spotify.search({
+//         type: "track",
+//         query: "The Sign"
+//       }, function (err, data) {
+//         if (err) {
+//           return console.log("Error: " + err);
+//         });
+//     }
+//   }
+//   .then(function (response) {
+//     console.log(response)
+//   });
+
+// } else {
+//   console.log.apply("else")
+//   spotify.search({
+//       type: "track",
+//       query: query
+//     })
+//     .then(function (response) {
+//       console.log(response);
+//       // console.log data (artist, song's name, preview lin of song from Spotify, album name)
+//     });
+// }
+// }
 
 
 
 // search for movie
 // command for search is node liri.js movie-this <insert movie name>
 var movie = function (query) {
-  var movieTitle = process.argv[3];
+  var movieTitle = process.argv.slice(3).join("+");
   // store api url in variable
-  var queryMovie = "http://www.omdbapi.com/?t=" + movieTitle + "&apikey=trilogy&"
-  var nobody = "http://www.omdbapi.com/?t=mr+nobody&apikey=trilogy&"
-
-  // create default for no input - if user input === null
-  if (!query) {
-    // use axios.get to pull info for "Mr. Nobody"
-    axios.get(nobody).then(function (response) {
-
-      console.log("Title: " + data.Title + "\nYear Released: " + data.Year + "\nIMDB Rating: " + data.imdbRating + "\nRotten Tomatos Rating: " + data.Ratings +
-        "\nCountry: " + data.Country + "\nLanguage: " + data.Language + "\nSummary: " + data.Plot + "\nCast: " + data.Actors);
-    });
-  } else {
-    // use axios.get with query variable
-    axios.get(queryMovie).then(function (response) {
-      var data = response.data;
-      console.log("Title: " + data.Title + "\nYear Released: " + data.Year + "\nIMDB Rating: " + data.imdbRating + "\nRotten Tomatos Rating: " + data.Ratings +
-        "\nCountry: " + data.Country + "\nLanguage: " + data.Language + "\nSummary: " + data.Plot + "\nCast: " + data.Actors);
-      // console.log data (title, year, IMDB rating, Rotten Tomatoes rating, country, language, plot, actors)
-    });
+  var URL = "http://www.omdbapi.com/?t=" + movieTitle + "&apikey=trilogy&"
+  // var nobody = "http://www.omdbapi.com/?t=mr+nobody&apikey=trilogy&"
+  // create default for no input
+  if (!movieTitle) {
+    movieTitle = "Mr+Nobody";
   }
-
+  // use axios.get with query variable
+  axios.get(URL).then(function (response, err) {
+      if (err) {
+        return console.log("Error: " + err);
+      }
+      data = response.data;
+      console.log("\n----Movie Info----\n" +
+        "\nTitle: " + data.Title +
+        "\nYear Released: " + data.Year +
+        "\nIMDB Rating: " + data.imdbRating +
+        "\nRotten Tomatoes Rating: " + data.Ratings[1].Value +
+        "\nCountry: " + data.Country +
+        "\nLanguage: " + data.Language +
+        "\nSummary: " + data.Plot +
+        "\nCast: " + data.Actors);
+      // console.log data (title, year, IMDB rating, Rotten Tomatoes rating, country, language, plot, actors)
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
 }
+
+
 
 // use random.txt to call command
 // command is node liri.js do-what-it-says
